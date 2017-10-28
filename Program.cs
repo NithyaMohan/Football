40443701a95874b206993e2c838111d761e84d1e
Nithya Mohanrajh <v.nithiya@gmail.com>
2017-10-29 06:53:34 +1100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Football
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+
+                var lines = File.ReadAllLines(@"C:\Users\Nithya\Documents\Visual Studio 2017\Projects\Football\Football\football.csv").Skip(1).Select(x => x.Split(',')).ToList();
+
+                var teamScores = new List<Scores>();
+
+                lines.ForEach(x => teamScores.Add(new Scores(x[0], int.Parse(x[1]), int.Parse(x[2]), int.Parse(x[3]), int.Parse(x[4]), int.Parse(x[5]), x[6], int.Parse(x[7]), int.Parse(x[8]))));
+
+                var team = teamScores.OrderBy(x => x.Difference).Select(x => x.TeamName).FirstOrDefault();
+                Console.WriteLine($"\n The team with smallest difference in 'for' and 'against' goals is : {team.ToString()}");
+                Console.ReadKey();
+
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"The following Exception has occured : \n {ex}");
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Scores.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Football
+{
+    internal class Scores
+    {
+        private int _teamNumber;
+        private string _teamName;
+        private int _p;
+        private int _w;
+        private int _l;
+        private int _d;
+        private int _f;
+        private string _hyphen;
+        private int _a;
+        private int _points;
+        private double _difference;
+
+        public Scores(string teamName, int p, int w, int l, int d, int f, string hyphen, int a, int points)
+        {
+            var teamNoName = teamName.Split('.');
+            TeamNumber = int.Parse(teamNoName[0]);
+            TeamName = teamNoName[1];
+            P = p;
+            W = w;
+            L = l;
+            D = d;
+            F = f;
+            Hyphen = hyphen;
+            A = a;
+            Points = points;
+            Difference = f - a;
+        }
+        #region Properties
+        public int TeamNumber
+        {
+            get { return _teamNumber; }
+            set { _teamNumber = value; }
+        }
+
+        public string TeamName
+        {
+            get { return _teamName; }
+            set { _teamName = value; }
+        }
+
+        public int P
+        {
+            get { return _p; }
+            set { _p = value; }
+        }
+
+        public int W
+        {
+            get { return _w; }
+            set { _w = value; }
+        }
+
+        public int L
+        {
+            get { return _l; }
+            set { _l = value; }
+        }
+
+        public int D
+        {
+            get { return _d; }
+            set { _d = value; }
+        }
+
+        public int F
+        {
+            get { return _f; }
+            set { _f = value; }
+        }
+
+        public string Hyphen
+        {
+            get { return _hyphen; }
+            set { _hyphen = value; }
+        }
+
+        public int A
+        {
+            get { return _a; }
+            set { _a = value; }
+        }
+
+        public int Points
+        {
+            get { return _points; }
+            set { _points = value; }
+        }
+
+        public double Difference
+        {
+            get { return _difference; }
+            set { _difference = value; }
+        }
+        #endregion
+
+
+
+    }
+}
